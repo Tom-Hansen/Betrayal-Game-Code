1f7c576de5a09a90812b0e66f742db6a4ecd5a64
Tom-Hansen <143756451+Tom-Hansen@users.noreply.github.com>
2023-11-08 16:20:47 -0500

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -14,7 +14,7 @@ int main()
 int x;
 
 {
-  std::cout << "Welcome to the digital version of Betrayal in the house on the hill! To start please select how many players are participating? (2-5 Players) \n";
+  std::cout << "Welcome to the digital version of Betrayal at house on the hill! To start please select how many players are participating? (2-5 Players) \n";
 
 // code to select how many players goes here
 cin >> x; // Get user input
@@ -32,3 +32,24 @@ cout << "Number of players selected is " << x; // Display the # of players
   }
   
 }
+
+
+// First half of the game
+
+//Trigger a turn
+
+//Role movement dice
+
+//Discover new room
+  //Trigger custom room tile
+  //trigger item, event, or omen
+    //if roll is needed, cause a roll and determine outcome of player roll
+    //add item or physical/mental damage or adding
+
+//return to turn trigger
+
+ // Second half of the game
+//Omen is decided (give like 3 options lmao)
+  //Add a turn for the monster if applicable
+      //monster is given a custom turn different from the humans
+//Add combat to players' turn
